bf12239abdd4087fff47db4bb0eac595b529ec81
DSaddam <128623988+DSaddam@users.noreply.github.com>
2023-03-26 00:53:04 +0530

    view.js code change

--- fronted/src/components/View.js
+++ fronted/src/components/View.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+
 import Container from "react-bootstrap/esm/Container";
 import NftCard from "./Card"
 
-class View extends Component {
-  render() {
-    return (
-      <Container>
-          <h2>NFT Drop</h2>
-          <NftCard />
-          <NftCard />
-      </Container>
-    );
-  }
+function View() {
+  return (
+    <Container>
+      <h2>NFT Drop</h2>
+      <NftCard />
+    </Container>
+  );
 }
+
 
 export default View;
